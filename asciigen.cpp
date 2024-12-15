3476052a42236e543f0a931690be4bc5a09b42c2
Filip Petrovic <filippetrovic603@gmail.com>
2024-12-15 21:35:48 +0100

    Everything for now added, when compiled works as intended

--- asciigen.cpp
+++ asciigen.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <stdint.h>
 #include <Magick++.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 int** convertImgToBrightness(Magick::Image image, int height, int width)
 {
@@ -27,37 +29,65 @@ int** convertImgToBrightness(Magick::Image image, int height, int width)
     return newImg;
 }
 
-/* TODO - add usage of paths and argument vector
- *      - remove resized image after running (during cleanup)
+void getTermSize(int &cols, int &rows)
+{
+    struct winsize w;
+
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    cols = w.ws_col;
+    rows = w.ws_row;
+}
+
+/* TODO - remove resized image after running (during cleanup)
  *      - research if theres a possiblility to make scaling dynamic based on image resolution
 */
 int main(int argc, char **argv)
 {
+    // Taking path from argument vector
+    std::string path {};
+
+    if (argc == 2) {
+        path = argv[1];
+    } else {
+        std::cerr << "Check if you entered path to image!" << std::endl;
+        return 1;
+    }
+
+    // Variables
     std::string ascii {"`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"};
     const int asciiNum = ascii.size();
 
     unsigned int width {0}, height {0};
-    const int resizeFactor = 3;
-
-    Magick::InitializeMagick(*argv);
 
     // Importing an image
+    Magick::InitializeMagick(*argv);
     Magick::Image image;
 
-    image.read("../res/test.jpg");
+    image.read(path);
 
     width = image.columns();
     height = image.rows();
 
-    // Resizing the image
-    unsigned int newWidth {width / resizeFactor};
-    unsigned int newHeight {height / resizeFactor};
+    // Resizing based on terminal & image
+    int termWidth = 0, termHeight = 0;
+    getTermSize(termWidth, termHeight);
+
+    size_t asciiWidth = termWidth - 10;
+    size_t asciiHeight = termHeight - 5;
+
+    double widthScale = static_cast<double>(image.columns() / asciiWidth);
+    double heightScale = static_cast<double>(image.rows() / asciiHeight);
+    double scalingFactor = std::min(widthScale, heightScale);
+
+    size_t newWidth = static_cast<size_t>(image.columns() / scalingFactor);
+    size_t newHeight = static_cast<size_t>(image.rows() / scalingFactor);
 
     image.resize(Magick::Geometry(newWidth, newHeight));
-    image.write("../res/resized.jpg");
+    image.write("./resized.jpg");
 
     Magick::Image resizedImage;
-    resizedImage.read("../res/resized.jpg");
+    resizedImage.read("./resized.jpg");
 
     height = newHeight;
     width = newWidth;
@@ -76,6 +106,8 @@ int main(int argc, char **argv)
     {
         for (int w = 0; w < width; w++)
         {
+            // prints a char 3 times to improve clarity,
+            // but probably at cost of performance (easier to append)
             std::cout << mapToAscii(newImg[h][w]);
             std::cout << mapToAscii(newImg[h][w]);
             std::cout << mapToAscii(newImg[h][w]);
@@ -92,6 +124,12 @@ int main(int argc, char **argv)
 
     delete [] newImg;
     newImg = 0;
+
+    try {
+        std::remove("./resized.jpg") == 0;
+    } catch (...) {
+        std::perror("Error removing resized image file.\n");
+    }
 
     return 0;
 }
